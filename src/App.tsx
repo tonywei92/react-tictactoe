7c987c024b6966ef6f3bb932423baa9bb8863e94
Tony Song <tonywei92@gmail.com>
2020-08-14 02:43:03 +0700

    chore: setup web router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from 'react-router-dom';
+import HomePage from './components/pages/Home';
+import NewGamePage from './components/pages/NewGame';
+import GamePage from './components/pages/Game';
+import 'tailwindcss/dist/base.min.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/newgame" exact component={NewGamePage} />
+          <Route path="/game" exact component={GamePage} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
