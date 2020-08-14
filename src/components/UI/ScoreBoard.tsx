61261bb59a8c1a619f4aecdee42a1514aa175848
Tony Song <tonywei92@gmail.com>
2020-08-14 17:54:43 +0700

    feat: add scoreboard component

---
+++ src/components/UI/ScoreBoard.tsx
@@ -0,0 +1,31 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import tw from 'twin.macro';
+
+interface Score {
+  label: string;
+  score: number;
+}
+
+interface Props {
+  scores?: Score[];
+}
+
+const ScoreBoard: React.FC<Props> = (props) => {
+  const { scores = [] } = props;
+  return (
+    <div css={tw`flex mb-8 bg-white shadow border rounded`}>
+      {scores.map((score) => (
+        <div
+          key={score.label}
+          tw="px-2 border-r w-24 flex p-2 flex-col items-center"
+        >
+          <div tw="text-indigo-700 text-4xl font-bold">{score.score}</div>
+          <div tw="text-gray-600 font-bold text-xs">{score.label}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ScoreBoard;
