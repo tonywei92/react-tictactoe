da14691265a82d5f1170c47c31be5f4a0c34bc56
Tony Song <tonywei92@gmail.com>
2020-08-14 22:52:10 +0700

    chore: remove unused console.log

--- src/components/TicTacToe/utils.tsx
+++ src/components/TicTacToe/utils.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export const createGameBoard = (gridSize: number): Array<string[]> => {
-  console.log('asdadsd');
   return Array(gridSize)
     .fill(null)
     .map(() => Array(gridSize).fill(''));
