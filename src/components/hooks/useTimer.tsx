84dcedf2033e345f40da5f09974c65c92d23f7db
Tony Song <tonywei92@gmail.com>
2020-08-14 22:09:23 +0700

    fix: useTimer wrong calculation

--- src/components/hooks/useTimer.tsx
+++ src/components/hooks/useTimer.tsx
@@ -7,7 +7,7 @@ interface UseTimerInterface {
   percentProgress: number;
 }
 
-const useTimer = (time = 10, callback?: () => void): UseTimerInterface => {
+const useTimer = (time: number, callback?: () => void): UseTimerInterface => {
   const [start, setStart] = useState(false);
   const [progress, setProgress] = useState(0);
   const [percentProgress, setPercentProgress] = useState(0);
@@ -16,17 +16,17 @@ const useTimer = (time = 10, callback?: () => void): UseTimerInterface => {
     let timer = 0;
     if (start) {
       timer = window.setInterval(() => {
-        const unit = time / 100;
-        setProgress(progress + time / 100);
-        setPercentProgress(Math.ceil(progress / unit));
-        if (progress + time / 100 >= time) {
+        const unit = 1;
+        setProgress(progress + unit);
+        setPercentProgress(Math.ceil((100 / time) * (progress + 1)));
+        if (progress + unit >= time + unit) {
           setStart(false);
           clearInterval(timer);
           if (callback) {
             callback();
           }
         }
-      }, 100);
+      }, 1000);
     }
     return (): void => {
       clearInterval(timer);
