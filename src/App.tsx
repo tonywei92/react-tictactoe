1c067ff9f3ed3d02d1f937921d75bcd7a0525e88
Tony Song <tonywei92@gmail.com>
2020-08-14 23:28:39 +0700

    feat: add 404 page and router address prefix

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,19 @@ import HomePage from './components/pages/Home';
 import NewGamePage from './components/pages/NewGame';
 import GamePage from './components/pages/Game';
 import AboutPage from './components/pages/About';
+import p404Page from './components/pages/404';
 import 'tailwindcss/dist/base.min.css';
 
 const App: React.FC = () => {
   return (
     <Provider store={gameStore}>
-      <Router>
+      <Router basename="/react-tictactoe">
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/newgame" exact component={NewGamePage} />
           <Route path="/game" exact component={GamePage} />
           <Route path="/about" exact component={AboutPage} />
+          <Route path="/*" exact component={p404Page} />
         </Switch>
       </Router>
     </Provider>
