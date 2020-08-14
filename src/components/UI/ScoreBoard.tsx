cacfb0c881e6de26e940e3d5919dcc145a2bb6ad
Tony Song <tonywei92@gmail.com>
2020-08-14 22:15:23 +0700

    style: add responsiveness to ScoreBoard

--- src/components/UI/ScoreBoard.tsx
+++ src/components/UI/ScoreBoard.tsx
@@ -14,13 +14,15 @@ interface Props {
 const ScoreBoard: React.FC<Props> = (props) => {
   const { scores = [] } = props;
   return (
-    <div css={tw`flex mb-8 bg-white shadow border rounded`}>
+    <div css={tw`flex mb-4 md:mb-8 bg-white shadow border rounded`}>
       {scores.map((score) => (
         <div
           key={score.label}
           tw="px-2 border-r w-24 flex p-2 flex-col items-center"
         >
-          <div tw="text-indigo-700 text-4xl font-bold">{score.score}</div>
+          <div tw="text-indigo-700 text-xl md:text-4xl font-bold">
+            {score.score}
+          </div>
           <div tw="text-gray-600 font-bold text-xs">{score.label}</div>
         </div>
       ))}
