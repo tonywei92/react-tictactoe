057ab5de8a8401fe9c8a7edb48f87af623296c6c
Tony Song <tonywei92@gmail.com>
2020-08-14 22:09:56 +0700

    feat: add footer to main layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -7,12 +7,33 @@ interface Props {
 }
 
 const style = css`
-  ${tw`container mx-auto items-center flex flex-col`}
+  ${tw`items-center flex flex-col h-screen`}
+`;
+
+const footerStyle = css`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 18px;
+  ${tw`bg-indigo-500 border-indigo-600 border-t-2 flex text-white justify-center items-center text-xs font-bold`}
 `;
 
 const MainLayout: React.FC<Props> = (props) => {
   const { children } = props;
-  return <div css={style}>{children}</div>;
+  return (
+    <div css={style}>
+      {children}
+      <div css={footerStyle}>
+        <span>
+          Made with&nbsp;
+          <span role="img" aria-label="love symbol">
+            ❤️
+          </span>
+          &nbsp; by Tony Song
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
