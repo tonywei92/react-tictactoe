895d03a9d7d417f3ec885fa6c75cc5d3eb76b112
Tony Song <tonywei92@gmail.com>
2020-08-14 23:17:57 +0700

    Updates

---
+++ precache-manifest.223318e02486634fc6d4d981a263825a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9277f3c127fa2b05a2084a128e34a9c5",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "40c26e26b87210b722dd",
+    "url": "/react-tictactoe/static/css/2.3c25a767.chunk.css"
+  },
+  {
+    "revision": "d41a8c9f8b15b5b012fe",
+    "url": "/react-tictactoe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40c26e26b87210b722dd",
+    "url": "/react-tictactoe/static/js/2.a366dfac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tictactoe/static/js/2.a366dfac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41a8c9f8b15b5b012fe",
+    "url": "/react-tictactoe/static/js/main.2de848dc.chunk.js"
+  },
+  {
+    "revision": "9e7d94a0a69203f46dea",
+    "url": "/react-tictactoe/static/js/runtime-main.813f326f.js"
+  },
+  {
+    "revision": "d48f32dbf01207a452f262d2cfa54f97",
+    "url": "/react-tictactoe/static/media/bell.d48f32db.mp3"
+  },
+  {
+    "revision": "afbfadcac18c4940768282ca9fa6b309",
+    "url": "/react-tictactoe/static/media/click.afbfadca.mp3"
+  },
+  {
+    "revision": "9aa0cbe8f39a4c2eb78a16909070d8a4",
+    "url": "/react-tictactoe/static/media/fail.9aa0cbe8.mp3"
+  }
+]);
