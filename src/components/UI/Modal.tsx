813bcea919ef7829ed84d559d8ee78c5696ca6df
Tony Song <tonywei92@gmail.com>
2020-08-14 22:14:56 +0700

    style: add border to modal

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -10,7 +10,7 @@ interface Props {
   icon?: React.ReactNode;
 }
 
-const style = tw`bg-white fixed rounded-lg overflow-hidden shadow-xl transform flex flex-col transition-all sm:max-w-md sm:w-full shadow-lg z-10`;
+const style = tw`bg-white fixed rounded-lg overflow-hidden shadow-xl transform flex flex-col transition-all sm:max-w-md sm:w-full shadow-lg z-10 border`;
 
 const Modal: React.FC<Props> = (props) => {
   const { children, icon, visible = false, buttons = [] } = props;
@@ -23,7 +23,7 @@ const Modal: React.FC<Props> = (props) => {
   }, [setSpring, visible]);
   return (
     <animated.div style={spring} css={style}>
-      <div tw="mt-3 mb-3 pt-5 pb-4 px-6 flex items-center">
+      <div tw="mt-3 mb-3 pt-5 pb-4 px-6 flex items-center text-xs md:text-lg">
         <div tw="mr-3">{icon}</div>
         <div>{children}</div>
       </div>
