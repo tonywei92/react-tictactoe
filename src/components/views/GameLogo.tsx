75acbd0c155128464a472603e6025dbf0c06ddcc
Tony Song <tonywei92@gmail.com>
2020-08-15 09:21:13 +0700

    style: change logo text to normal weight

--- src/components/views/GameLogo.tsx
+++ src/components/views/GameLogo.tsx
@@ -3,7 +3,7 @@ import { jsx } from '@emotion/core';
 import tw from 'twin.macro';
 
 const GameLogo = () => (
-  <div css={tw`font-bold text-indigo-100 text-lg md:text-2xl mx-4`}>
+  <div css={tw`text-indigo-100 text-lg md:text-2xl mx-4`}>
     TicTacToe v{process.env.REACT_APP_VERSION}
   </div>
 );
