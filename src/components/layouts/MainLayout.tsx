b9339b771135d79aa6739232885c150a32fbb515
Tony Song <tonywei92@gmail.com>
2020-08-15 09:01:00 +0700

    style: update footer style to thinner font and higher container

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -14,8 +14,8 @@ const footerStyle = css`
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 18px;
-  ${tw`bg-indigo-500 border-indigo-600 border-t-2 flex text-white justify-center items-center text-xs font-bold`}
+  height: 20px;
+  ${tw`bg-indigo-500 border-indigo-600 border-t-2 flex text-white justify-center items-center text-xs font-light`}
 `;
 
 const MainLayout: React.FC<Props> = (props) => {
