e8c3ef92e4cc937dd3aead55379db3b94fae6195
Tony Song <tonywei92@gmail.com>
2020-08-15 11:39:26 +0700

    chore: add right button props to NavBar

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -6,10 +6,11 @@ import tw from 'twin.macro';
 
 interface Props {
   leftButton?: React.ReactNode;
+  rightButton?: React.ReactNode;
 }
 
 const NavBar: React.FC<Props> = (props) => {
-  const { leftButton } = props;
+  const { leftButton, rightButton } = props;
   return (
     <nav
       css={tw`py-3 md:py-2 mb-4 shadow border-b-4 border-indigo-600 flex w-full px-4  justify-center bg-indigo-500`}
@@ -17,6 +18,7 @@ const NavBar: React.FC<Props> = (props) => {
       <div tw="w-full md:w-8/12 lg:w-6/12 flex justify-center relative">
         <div tw="absolute left-0">{leftButton}</div>
         <GameLogo />
+        <div tw="absolute right-0">{rightButton}</div>
       </div>
     </nav>
   );
