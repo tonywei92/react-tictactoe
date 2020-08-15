0590992bc0995987235e322f225dfda22ef8ba2c
Tony Song <tonywei92@gmail.com>
2020-08-15 10:48:25 +0700

    fix: reduce iOS audio delay

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const audioCtx = new AudioContext();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
